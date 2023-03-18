ac9865d5fc773777cd487e3692b0e29dfec06bbe
NicodemosSantos <111062089+NicodemosSantos@users.noreply.github.com>
2023-03-18 00:48:33 +0000

    converted implementation to FlatList

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,4 +1,4 @@
-import { Text, View, TextInput, TouchableOpacity, ScrollView } from 'react-native';
+import { Text, View, TextInput, TouchableOpacity, FlatList } from 'react-native';
 
 import { Participant } from '../../components/Participant';
 import { styles } from './styles';
@@ -35,15 +35,23 @@ export default function Home() {
         </TouchableOpacity>
       </View>
 
-      <ScrollView showsVerticalScrollIndicator={false}>
-        {participants.map((participant, index) => (
+      <FlatList
+        data={[]}
+        keyExtractor={item => item}
+        renderItem={({ item }) => (
           <Participant
-            key={index}
-            name={participant}
+            key={item}
+            name={item}
             onRemove={() => handleParticipantRemove("savinho")}
           />
-        ))}
-      </ScrollView>
+        )}
+        showsVerticalScrollIndicator={false}
+        ListEmptyComponent={() => (
+          <Text style={styles.listEmptyText}>
+            Ninguém chegou no evento ainda? Adicione um participante
+          </Text>
+        )}
+      />
 
     </View>
   )
